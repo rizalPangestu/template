e9897696be80fcffa30248343bf7aee0ffefc4c2
rizalPangestu <rizalpangestu4@gmail.com>
2019-10-31 20:31:21 +0700

    modify css and header

--- template1/template/header.php
+++ template1/template/header.php
@@ -7,33 +7,58 @@
     </head>
 <body>
     <header>
-        <nav class="top-navbar" class="top-navbar">
+        <nav id="top-navbar" class="top-navbar">
             <div class="navabar-container ">
                 <div class="navbar-content">
                     <div class="navbar-brand">
-                        <div class="brand">
-                            <h1>Wallpaper Anime</h1>
+                        <div class="brand-wrapper">
+                            <div class="row">
+                                <div class="brand">
+                                    <h1>Wallpaper Anime</h1>
+                                </div>
+                                <div class="sosmed">
+                                    <div class="sosmed-btn">
+                                        <ul class="sosmed-menu">
+                                            <li class="sosmed-item">
+                                            <a  class="sosmed-link" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
+                                            </li>
+                                            <li class="sosmed-item">
+                                            <a  class="sosmed-link" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
+                                            </li>
+                                            <li class="sosmed-item">
+                                            <a  class="sosmed-link" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="navbar-collapse">
-                        <div class="navbar-menu">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link "href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link "href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link "href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link "href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link "href="#">Home</a>
-                                </li>
-                            </ul>
+                        <div class="navbar-wrapper">
+                            <div class="navbar-menu">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link "href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link "href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link "href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link "href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link "href="#">Home</a>
+                                    </li>
+                                    <li class="ranpost">
+                                        <a class="ranpost-link"href="#">Your Waifu</a>
+                                    </li>
+                                    
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
