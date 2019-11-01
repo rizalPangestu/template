8857a11b826dcd6168e5cf868d53eb0b4efb2b92
justirva <justirva@gmail.com>
2019-11-01 11:31:04 +0700

    refactoring navbar

--- template1/template/header.php
+++ template1/template/header.php
@@ -6,65 +6,33 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     </head>
 <body>
-    <header>
-        <nav id="top-navbar" class="top-navbar">
-            <div class="navabar-container ">
-                <div class="navbar-content">
-                    <div class="navbar-brand">
-                        <div class="brand-wrapper">
-                            <div class="row">
-                                <div class="brand">
-                                    <h1>Wallpaper Anime</h1>
-                                </div>
-                                <div class="sosmed">
-                                    <div class="sosmed-btn">
-                                        <ul class="sosmed-menu">
-                                            <li class="sosmed-item">
-                                            <a  class="sosmed-link" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
-                                            </li>
-                                            <li class="sosmed-item">
-                                            <a  class="sosmed-link" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
-                                            </li>
-                                            <li class="sosmed-item">
-                                            <a  class="sosmed-link" href="#" title="Twitter"><i class="fa fa-twitter"></i></a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <header id="header">
+            <div class="header-container">
+                <div class="row">
+                    <div class="h-brand">
+                        <h2>CodeArt</h2>
                     </div>
-                    <div class="navbar-collapse">
-                        <div class="navbar-wrapper">
-                            <div class="navbar-menu">
-                                <ul class="navbar-nav">
-                                    <li class="nav-item">
-                                        <a class="nav-link "href="#">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link "href="#">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link "href="#">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link "href="#">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link "href="#">Home</a>
-                                    </li>
-                                    <li class="ranpost">
-                                        <a class="ranpost-link"href="#">Your Waifu</a>
-                                    </li>
-                                    
-                                </ul>
-                            </div>
-                        </div>
+                    <div class="h-social">
+                        <ul class="sosmed-wrap">
+                            <li class="sosmed-item"><a href="#">Facebook</a></li>
+                            <li class="sosmed-item"><a href="#">Twitter</a></li>
+                            <li class="sosmed-item"><a href="#">Instagram</a></li>
+                            <li class="sosmed-item"><a href="#">Pinterest</a></li>
+                        </ul>
                     </div>
                 </div>
+            </div>   
+            <div class="menu-container">
+              <nav id="nav-top" class="navbar">
+                <ul class="nav-wrap">
+                    <li class="nav-item"><a href="#">Menu 1</a></li>
+                    <li class="nav-item"><a href="#">Menu 2</a></li>
+                    <li class="nav-item"><a href="#">Menu 3</a></li>
+                    <li class="nav-item"><a href="#">Menu 4</a></li>
+                    <li class="nav-item waifu"><a href="#">Your Waifu/Husbando</a></li>
+                </ul>
+              </nav>
             </div>
-        </nav>
-    
     </header>
         
 
