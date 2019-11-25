b2e23e4945b96a44c0717bc6bbd79b924562c47f
rizalPangestu <rizalpangestu4@gmail.com>
2019-11-25 15:45:29 +0700

    Single Post & Modify CSS

---
+++ template1/singlepost.php
@@ -0,0 +1,121 @@
+<?php include("template/header.php") ?>
+<div class="main-container">
+    <div id="single-post" class="single_post-container">
+        <div class="content-container">
+            <div class="single_post-wrapper">
+                <div class="row">
+                    <div class="img-wrapper">
+                        <div class="post-img">
+                            <img src="./images/gambar1.jpg" alt="gambar1">
+                        </div>
+                    </div>
+                    <div class="content-wrapper">
+                        <div class="content-post">
+                            <div class="btn-menu">
+                                <div class="row">
+                                    <div class="btn-dots">
+                                        <div >
+                                            <a class="dots" href="#">...</a>
+                                        </div>
+                                    </div>
+                                    <div class="btn-upload">
+                                        <div >
+                                            <a class="upload" href="#">Upload</a>
+                                        </div>
+                                    </div>
+                                    <div class="btn-save">
+                                        <div >
+                                            <a class="save" href="#">Save</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="uploader-wrapper">
+                                <div class="uploader">
+                                    <div class="uploader-name">
+                                        <h1>Uploaded by Who?</h1>
+                                    </div>
+                                    <div class="title-post">
+                                        <div class="title">
+                                            <h1>オッパイ</h1>
+                                        </div>
+                                    </div>
+                                    <div class="secondary-info-uploader">
+                                        <div class="info-uploader">
+                                            <div class="row">
+                                                <div class="info-img">
+                                                    <img src="./images/gambar1.jpg" alt="info">
+                                                </div>
+                                                <div class="uploader-name">
+                                                    <p>Who?</p>
+                                                </div>
+                                                <div class="btn-follow">
+                                                    <a  class="follow" href="#">Follow</a>
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="comments-wrapper">
+                                <div class="btn-comments">
+                                    <div class="row">
+                                        <div class="photos-wrapper">
+                                            <div class="add-photos"> 
+                                                <div class="btn-photos">
+                                                <a class="btn" href="#">Photos</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="comment-wrapper">
+                                            <div class="add-comment">
+                                                <div class="btn-comment">
+                                                    <a class="btn" href="#">Comment</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="comments-content">
+                                        <div class="comment-text">
+                                            <div class="text">
+                                                <p>Tried This Pin?</p>
+                                                <p>Add photos and show how it went</p>
+                                            </div>
+                                        </div>
+                                        <div class="btn-upload">
+                                            <div >
+                                                <a class="upload" href="#">Upload</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="history-wrapper">
+                                <div class="row">
+                                    <div class="info-img">
+                                        <img src="./images/gambar1.jpg" alt="info">
+                                    </div>
+                                    <div class="uploader-name">
+                                        <p>Who?</p>
+                                    </div>
+                                    <div class="uploader-history">
+                                        <div class="history">
+                                          <span class="row">
+                                            <p>saved to </p>
+                                            <p class="title">オッパイ</p> 
+                                          </span>
+                                           
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php include('template/footer.php') ?> 
